e6d06632e16f0bbab5c46f34e6d5b8ebb78a56a7
Usama Ajmal <30524475+usamaajmal@users.noreply.github.com>
2018-07-20 22:25:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const bodyParser = require('body-parser');
 const user = require('./routes/users');
 const mongoose = require('mongoose');
 
+
+//Port
 const PORT = 3000;
 
-
+//Body-Parser
  app.use(bodyParser.urlencoded({ extended: false }));
  app.use(bodyParser.json());
  
+//Routes
  app.use('/user', user);
 
+//Server Run
 app.listen(PORT, function(){
    console.log('Server is running on Port',PORT);
 });
 
+//Mongo DB Connection from mlab
 mongoose.connect('mongodb://root:toor321@ds243441.mlab.com:43441/jwt-authentication').then(
      console.log("DB Connected")
  );
